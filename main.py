686aa11fcd001e3d37d1537094aab507597aff98
lin6165288 <lin6165288@gmail.com>
2026-09-09 14:06:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+
+app = FastAPI(
+    title="橘貓代購後台"
+)
+
+app.mount(
+    "/static",
+    StaticFiles(directory="static"),
+    name="static"
+)
+
+templates = Jinja2Templates(
+    directory="templates"
+)
+
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
